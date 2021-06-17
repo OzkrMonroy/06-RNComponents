efe5a225e180af1fc07ac86eca4968616ea7eec2
Oscar Monroy <oscarmonroy657@gmail.com>
2021-06-17 11:21:12 -0600

    Icons have been configured to use Material Icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export const HomeScreen = () => {
   return (
