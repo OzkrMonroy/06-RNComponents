a0ce7bf93ba11a26b44ade57dc1b2b4d074415f6
Oscar Monroy <oscarmonroy657@gmail.com>
2021-06-25 13:36:50 -0600

    Component to display an Alert has been created. View AlertScreen.tsx file for an example of using the Alert component.

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -4,4 +4,5 @@ export const menuItems: MenuItem[] = [
   {name: 'Animation101', icon: 'animation', screen: 'Animation101Screen'},
   {name: 'Animation102', icon: 'auto-awesome-motion', screen: 'Animation102Screen'},
   {name: 'Switches', icon: 'toggle-on', screen: 'SwitchScreen'},
+  {name: 'Alerts', icon: 'alert', screen: 'AlertScreen'},
 ];
--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -4,6 +4,7 @@ import { HomeScreen } from '../screens/HomeScreen';
 import { Animation102Screen } from '../screens/Animation102Screen';
 import { Animation101Screen } from '../screens/Animation101Screen';
 import { SwitchScreen } from '../screens/SwitchScreen';
+import { AlertScreen } from '../screens/AlertScreen';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +22,7 @@ export const StackNavigation = () => {
       <Stack.Screen name='Animation101Screen' component={Animation101Screen}/>
       <Stack.Screen name='Animation102Screen' component={Animation102Screen}/>
       <Stack.Screen name='SwitchScreen' component={SwitchScreen}/>
+      <Stack.Screen name='AlertScreen' component={AlertScreen}/>
     </Stack.Navigator>
   )
 }
