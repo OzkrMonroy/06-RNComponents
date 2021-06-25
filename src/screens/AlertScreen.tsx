69b6af23ac9c961a7415d24cd497379d2466bee2
Oscar Monroy <oscarmonroy657@gmail.com>
2021-06-25 14:13:01 -0600

    A prompt for Android & IOS has been implemented.

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Button, Alert } from 'react-native';
+
+import prompt from 'react-native-prompt-android';
+
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
 export const AlertScreen = () => {
-  
+
   const showAlert = () => {
     Alert.alert(
       "Alert Title",
@@ -22,21 +25,37 @@ export const AlertScreen = () => {
   }
 
   const showPrompt = () => {
-    Alert.prompt(
-      'Prompt example',
-      'This is a prompt example.',
-      (text: string) => console.log(`info: ${text}`),
-      'plain-text',
-      'Hello!',
-      ''
+    // Alert.prompt(
+    //   'Prompt example',
+    //   'This is a prompt example.',
+    //   (text: string) => console.log(`info: ${text}`),
+    //   'plain-text',
+    //   'Hello!',
+    //   ''
+    // );
+
+
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder'
+      }
     );
   }
 
   return (
     <View style={styles.globalMargin}>
-      <HeaderTitle title='Alerts'/>
-      <Button title='Show alert' onPress={showAlert}/>
-      <Button title='Show prompt' onPress={showPrompt}/>
+      <HeaderTitle title='Alerts' />
+      <Button title='Show alert' onPress={showAlert} />
+      <Button title='Show prompt' onPress={showPrompt} />
     </View>
   )
 }
